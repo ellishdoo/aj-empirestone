608fe2b580ecd781fe6b6b218509689e3cd943a3
ellishdoo <eli.ellishd@gmail.com>
2026-05-11 11:59:13 -0700

    Allow Netlify frontend in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:3000",
         "http://localhost:3000",
+        "https://your-netlify-site-name.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
